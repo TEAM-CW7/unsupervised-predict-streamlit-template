1c85f31e0699c84474d06a9a4ecec3c7b32a0346
moolivieJHB <67946957+moolivieJHB@users.noreply.github.com>
2022-07-24 16:10:48 +0200

    Add files via upload
    App modifications

--- unsupervised-predict-streamlit-template/edsa_recommender.py
+++ unsupervised-predict-streamlit-template/edsa_recommender.py
@@ -55,8 +55,8 @@ def main():
         {'icon': "far fa-copy", 'label':"Left End"},
         # {'id':'Copy','icon':"🐙",'label':"Copy"},
         {'id':'eda', 'icon': "far fa-chart-bar", 'label':"Chart"},#no tooltip message
-        {'icon': "far fa-address-book", 'label':"Book"},
-        {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
+        {'icon': "far fa-address-book", 'label':"About"},
+        # {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
         {'icon': "far fa-clone", 'label':"Component"},
         {'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"}, #can add a tooltip message
         {'icon': "far fa-copy", 'label':"Right End"},
--- unsupervised-predict-streamlit-template/visuals.py
+++ unsupervised-predict-streamlit-template/visuals.py
@@ -10,9 +10,11 @@ import plotly.graph_objects as go
 
 # Importing data
 movies_df = pd.read_csv('resources/data/movies.csv',sep = ',')
-imdb_data = pd.read_csv('resources/data/imdb_data.csv')
+imdb_df = pd.read_csv('resources/data/imdb_data.csv')
 ratings_df = pd.read_csv('resources/data/ratings.csv')
-# eda_df = train_df[train_df['userId']!=72315]
+train_df = pd.read_csv('resources/data/train.csv')
+
+eda_df = train_df[train_df['userId']!=72315]
 ratings_df.drop(['timestamp'], axis=1,inplace=True)
 
 def feature_frequency(df, column):
@@ -60,46 +62,94 @@ def feature_count(df, column):
     plt.xlabel('Count')
     st.pyplot(fig)
 
-# def mean_calc(feat_df, ratings = eda_df, movies = movies_df, metadata = imdb_df, column = 'genres'):
+def mean_calc(feat_df, ratings = eda_df, movies = movies_df, metadata = imdb_df, column = 'genres'):
     
     
     
-#     mean_ratings = pd.DataFrame(ratings.join(movies, on='movieId', how='left').groupby(['movieId'])['rating'].mean())
-#     movie_eda = movies.copy()
-#     movie_eda = movie_eda.join(mean_ratings, on = 'movieId', how = 'left')
+    mean_ratings = pd.DataFrame(ratings.merge(movies, on='movieId', how='left').groupby(['movieId'])['rating'].mean())
+    movie_eda = movies.copy()
+    movie_eda = movie_eda.join(mean_ratings, on = 'movieId', how = 'left')
 
-#     # Exclude missing values
-#     movie_eda = movie_eda
-#     movie_eda2 = movie_eda[movie_eda['rating'].notnull()]
+    # Exclude missing values
+    movie_eda = movie_eda
+    movie_eda2 = movie_eda[movie_eda['rating'].notnull()]
 
-#     means = []
-#     for feat in feat_df[f'{column}']:
-#         mean = round(movie_eda2[movie_eda2[f'{column}'].str.contains(feat)]['rating'].mean(),2)
-#         means.append(mean)
-#     return means
+    means = []
+    for feat in feat_df[f'{column}']:
+        mean = round(movie_eda2[movie_eda2[f'{column}'].str.contains(feat)]['rating'].mean(),2)
+        means.append(mean)
+    return means
 
-# def genre_popularity(df):
-#     """
-#     Plots the mean rating per genre.
-#     """
-#     count_filt = 500
-#     plt.figure(figsize=(10,6))
-#     plot_data = df[df['count']>count_filt]
-#     mean = plot_data['mean_rating'].mean()
-#     min_ = plot_data['mean_rating'].min()
-#     max_ = plot_data['mean_rating'].max()
-#     sns.barplot(y = plot_data['genres'], x = plot_data['mean_rating'], order = plot_data['genres'], orient='h',palette='cool')
-#     plt.axvline(x=mean, label = f'mean {round(mean,1)}' , color='black', lw=1, ls ='--')
-#     plt.axvline(x=min_, label = f'min {round(min_,1)}' , color='#4D17A0', lw=1, ls = '--')
-#     plt.axvline(x=max_, label = f'max {max_}' , color='#4DA017', lw=1,ls = '--')
-#     plt.title(f'Mean Rating Per Genre', fontsize=14)
-#     plt.ylabel('Genre')
-#     plt.xlabel('Mean Rating')
-#     plt.legend(loc='lower center')
-#     plt.show()
+def genre_popularity(df):
+    """
+    Plots the mean rating per genre.
+    """
+    count_filt = 500
+    fig = plt.figure(figsize=(10,6))
+    plot_data = df[df['count']>count_filt]
+    mean = plot_data['mean_rating'].mean()
+    min_ = plot_data['mean_rating'].min()
+    max_ = plot_data['mean_rating'].max()
+    sns.barplot(y = plot_data['genres'], x = plot_data['mean_rating'], order = plot_data['genres'], orient='h',palette='cool')
+    plt.axvline(x=mean, label = f'mean {round(mean,1)}' , color='black', lw=1, ls ='--')
+    plt.axvline(x=min_, label = f'min {round(min_,1)}' , color='#4D17A0', lw=1, ls = '--')
+    plt.axvline(x=max_, label = f'max {max_}' , color='#4DA017', lw=1,ls = '--')
+    plt.title(f'Mean Rating Per Genre', fontsize=14)
+    plt.ylabel('Genre')
+    plt.xlabel('Mean Rating')
+    plt.legend(loc='lower center')
+    st.pyplot(fig)
+
+def ratings_distplot(df, column='rating'):
+    """
+    Plots the distribution of ratings in the dataset.
+    Parameters
+    ----------
+        df (DataFrame): input DataFrame
+        column (str): column to plot
+    Returns
+    -------
+        distplot (NoneType): distplot of rating frequencies
+    """
+    fig = plt.figure(figsize=(8,6))
+    ax = sns.distplot(df[f'{column}'],bins=10, kde=False, hist_kws=dict(alpha=0.6),color="#4D17A0")
+    mean = df[f'{column}'].mean()
+    median = df[f'{column}'].median()
+    plt.axvline(x=mean, label = f'mean {round(mean,2)}' , color='#4D17A0', lw=3, ls = '--')
+    plt.axvline(x=median, label = f'median {median}' , color='#4DA017', lw=3, ls = '--')
+    plt.xlim((0.5,5))
+    plt.ylim((0,2500000))
+    plt.title(f'Distribution of Ratings', fontsize=14)
+    plt.xlabel('Rating')
+    plt.ylabel('Frequency')
+    plt.legend()
+    st.pyplot(fig) 
+
+def mean_ratings_scatter(df, color='#F33DD8', column='userId'):
+    """
+    Make scatterplots of mean ratings.
+    Parameters
+    ----------
+        df (DataFrame): input DataFrame
+        color (str): plot colour
+        column (str): column to plot
+    Returns
+    -------
+        scatterplot (NoneType): scatterplot of mean number of ratings
+    """
+    fig = plt.figure(figsize=(6,4))
+    mean_ratings = df.groupby(f'{column}')['rating'].mean()
+    user_counts = df.groupby(f'{column}')['movieId'].count().values
+    sns.scatterplot(x=mean_ratings, y = user_counts, color=color)
+    plt.title(f'Mean Ratings by Number of Ratings', fontsize=14)
+    plt.xlabel('Rating')
+    plt.ylabel('Number of Ratings')
+    st.pyplot(fig) 
 
 def plot_eda():
     genres = feature_frequency(movies_df, 'genres')
     feature_count(genres.sort_values(by = 'count', ascending=False), 'genres')   
-    # genres['mean_rating'] = mean_calc(genres) 
-    # genre_popularity(genres.sort_values('mean_rating', ascending=False))
+    genres['mean_rating'] = mean_calc(genres) 
+    genre_popularity(genres.sort_values('mean_rating', ascending=False))
+    ratings_distplot(eda_df)
+    mean_ratings_scatter(eda_df,'#F33DD8')
